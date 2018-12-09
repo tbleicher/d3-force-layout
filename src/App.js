6750a8e9027c2e8ad62e974a1a231f6b3e000607
Thomas Bleicher <tbleicher@gmail.com>
2018-12-09 12:03:05 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-import BubbleChart from "./components/BubbleChart"
+import BubbleChart from './components/BubbleChart';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <BubbleChart />
+        <BubbleChart />
       </div>
     );
   }
